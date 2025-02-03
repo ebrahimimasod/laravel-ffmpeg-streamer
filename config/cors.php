cf46c5ae2352e61909f557e931934f5769956440
Masoud Ebrahimi <ebrahimimasod@gmail.com>
2025-02-03 12:25:28 +0330

    update

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://test.noorejan.com'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/^https:\/\/.*\.noorejan\.com$/'],
 
     'allowed_headers' => ['*'],
 
